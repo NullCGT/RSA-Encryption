fd9aa0313eb5238dd108280db704794617400dab
Connor L. Gregorich-Trevor <gregoric@cs.grinnell.edu>
2017-04-25 09:27:40 -0500

    Self chat skeleton

--- Testencrypt.c
+++ Testencrypt.c
@@ -10,6 +10,10 @@
 // This code is copied from https://shanetully.com/2012/04/simple-public-key-encryption-with-rsa-and-opensll/
 
 #define KEYBITS 4096
+// http://stackoverflow.com/questions/3585846/color-text-in-terminal-applications-in-unix
+#define GREEN "\x1B[32m"
+#define RED "\x1B[31m"
+#define RESET "\x1B[0m"
 
 int main (void)
 {
@@ -18,12 +22,17 @@ int main (void)
   FILE *file;
   BIO *private;
   BIO *public;
+  char* pri_key;
+  char* pub_key;
+
+  OpenSSL_add_all_algorithms();
+  system("clear");
   // http://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c-cross-platform
   if (access("Keys.txt", F_OK) != -1) {
     printf("Found keypair file!\n");
   } else {
     printf("Did not find keypair file. Generating new keypair...\n");
-
+    keypair = RSA_generate_key(KEYBITS, 3, NULL, NULL);
     private = BIO_new(BIO_s_mem());
     public = BIO_new(BIO_s_mem());
 
@@ -33,8 +42,8 @@ int main (void)
     size_t pri_len = BIO_pending(private);
     size_t pub_len = BIO_pending(public);
 
-    char *pri_key = malloc(pri_len + 1);
-    char *pub_key = malloc(pub_len + 1);
+    pri_key = malloc(pri_len + 1);
+    pub_key = malloc(pub_len + 1);
 
     BIO_read(private, pri_key, pri_len);
     BIO_read(public, pub_key, pub_len);
@@ -42,43 +51,64 @@ int main (void)
     pri_key[pri_len] = '\0';
     pub_key[pub_len] = '\0';
     
-    keypair = RSA_generate_key(KEYBITS, 3, NULL, NULL);
 
+    file = fopen("PUBKEY.txt", "w");
+    PEM_write_RSA_PUBKEY(file, keypair);
+    fclose(file);
   }
-  
+
+  // Welcome messages
+  printf("Welcome to the secure messanger client.\n");
+  printf("When you message someone else, your message will be sent securely over a server relay.\n");
+  printf("Type :help for help.\n\n");
   
 
   // Allocate memory for a message
-  char message[KEYBITS/8];
-  printf("Encrypt this: ");
-  fgets(message, KEYBITS/8, stdin);
-  message[strlen(message)-1] = '\0';
+  while(1 == 1) {
+    char message[KEYBITS/8];
+    fgets(message, KEYBITS/8, stdin);
+    message[strlen(message)-1] = '\0';
 
-  char *encrypted_message = malloc(RSA_size(keypair));
-  int encrypt_len;
-  char *err = malloc(130);
+    if (strcmp(message, ":logout") == 0) {
+      printf("Logging out...\n");
+      system("clear");
+      return 0;
+    } else if (strcmp(message, ":help") == 0) {
+      printf("\nCommands:"
+             "\n:logout     Quits the program.\n"
+             ":help       Displays command list.\n"
+             ":public     Prints your public key to the console.\n\n");
+    }
+    else if (strcmp(message, ":public") == 0) {
+        printf("\n%s\n\n", pub_key);
+    }
+    else {
 
-  if((encrypt_len = RSA_public_encrypt(strlen(message)+1, (unsigned char*) message,
-                                       (unsigned char*)encrypted_message, keypair, RSA_PKCS1_OAEP_PADDING)) == -1) {
-    ERR_load_crypto_strings();
-    ERR_error_string(ERR_get_error(), err);
-    fprintf(stderr, "Error encrypting message: %s\n", err);
-  }
+      char *encrypted_message = malloc(RSA_size(keypair));
+      int encrypt_len;
+      char *err = malloc(130);
+
+      if((encrypt_len = RSA_public_encrypt(strlen(message)+1, (unsigned char*) message,
+                                           (unsigned char*)encrypted_message, keypair, RSA_PKCS1_OAEP_PADDING)) == -1) {
+        ERR_load_crypto_strings();
+        ERR_error_string(ERR_get_error(), err);
+        fprintf(stderr,RED "Error encrypting message: %s\n" RESET, err);
+      }
 
 
   
-  char *decrypted_message = malloc(RSA_size(keypair));
-  if(RSA_private_decrypt(encrypt_len, (unsigned char*)encrypted_message, (unsigned char*)decrypted_message,
-                         keypair, RSA_PKCS1_OAEP_PADDING) == -1) {
-    ERR_load_crypto_strings();
-    ERR_error_string(ERR_get_error(),err);
-    fprintf(stderr, "Error decrypting message: %s\n", err);
-  } else {
-    printf("Decrypted message: %s\n", decrypted_message);
+      char *decrypted_message = malloc(RSA_size(keypair));
+      if(RSA_private_decrypt(encrypt_len, (unsigned char*)encrypted_message, (unsigned char*)decrypted_message,
+                             keypair, RSA_PKCS1_OAEP_PADDING) == -1) {
+        ERR_load_crypto_strings();
+        ERR_error_string(ERR_get_error(),err);
+        fprintf(stderr,RED "Error decrypting message: %s\n" RESET, err);
+      } else {
+        printf(GREEN "Decrypted message: %s\n" RESET, decrypted_message);
+      }
+    }
+
   }
-
-
-
   
   return 0;
 }
