1791b8a118dd31fa7d39ce4582e41eef1ac827fc
Siyu Zhang <zhangsiy@cs.grinnell.edu>
2017-05-09 15:57:35 -0500

    fixed

--- messaging/tcpclient.c
+++ messaging/tcpclient.c
@@ -23,7 +23,7 @@
 void act_as_client (tosend_t package);
 void act_as_middle_server (tosend_t package);
 void act_as_server (tosend_t package);
-void initialize_package(tosend_t package, int num_middle_servers, char* final_ip);
+void initialize_package(tosend_t *package, int num_middle_servers, char* final_ip);
 void* receiveMessage(void* socket); 
 
 //The user runs as a client. Takes a package with the encripted ip adress, decrypts
@@ -39,10 +39,10 @@ void act_as_client(tosend_t package) {
   pthread_t rThread;
   RSA* server_keypair;
   char* serverAddr;
-
+  
   server_keypair = do_bad_things(NULL);
+  printf("%s\n",package.ip[package.index]);
   struct_decryption(server_keypair, package, sizeof(server_keypair));
-
   serverAddr = (char*) malloc(sizeof(char)*16);
   strcpy(serverAddr, package.ip[package.index]);
   
@@ -173,10 +173,13 @@ void act_as_server(tosend_t package) {
 
 //Initialization of our package struct for cleanup
 //@returns void
-void initialize_package(tosend_t package, int num_of_middle_servers, char* final_ip) {
-  package.index = 0;
-  package.num_of_middle_servers = num_of_middle_servers;
-  strncpy(package.ip[num_of_middle_servers], final_ip, sizeof(char)*16);
+void initialize_package(tosend_t* package, int num_of_middle_servers, char* final_ip) {
+  package->index = 0;
+  package->num_of_middle_servers = num_of_middle_servers;
+  strncpy(package->ip[num_of_middle_servers], final_ip, sizeof(char)*16);
+  // printf("%s\n",final_ip);
+  // printf("%d\n",num_of_middle_servers);
+  // printf("%s\n",package.ip[num_of_middle_servers]);
 }
 
 //Takes in a string package from the socket. If given to the end server, it
@@ -211,11 +214,11 @@ int main(int argc, char**argv) {
 
   if (argc > 2) {
     relay_data = read_file(); //initializes a linked list containing ip addresses and RSA keys
-    initialize_package(package, atoi(argv[1]), (char*) argv[2]);
+    initialize_package(&package, atoi(argv[1]), (char*) argv[2]);
     //struct_encryption(relay_data,package, do_bad_things(argv[2]));
     act_as_client(package);
   } else {
-    initialize_package(package, 0 , "");
+    initialize_package(&package, 0 , "");
     act_as_server(package);
   }
   return 0;
